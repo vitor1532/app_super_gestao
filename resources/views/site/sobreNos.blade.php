25b51d1acad4aa4c4cf00616d1602c328cb40977
unknown <vitomartinsalmeida@gmail.com>
2022-05-12 14:19:13 -0300

    fazendo um template basico e aplicando

--- resources/views/site/sobreNos.blade.php
+++ resources/views/site/sobreNos.blade.php
@@ -1,56 +1,49 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <title>Super Gestão - Sobre Nós</title>
-        <meta charset="utf-8">
+@extends('site.layouts.basico')
 
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
-    </head>
 
-    <body>
-        <div class="topo">
+@section('conteudo')
+    <div class="topo">
 
-            <div class="logo">
-                <img src="img/logo.png">
-            </div>
-
-            <div class="menu">
-                <ul>
-                    <li><a href="{{ route('site.index') }}">Principal</a></li>
-                    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
-                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
-                </ul>
-            </div>
+        <div class="logo">
+            <img src="img/logo.png">
         </div>
 
-        <div class="conteudo-pagina">
-            <div class="titulo-pagina">
-                <h1>Olá, eu sou o Super Gestão</h1>
-            </div>
+        <div class="menu">
+            <ul>
+                <li><a href="{{ route('site.index') }}">Principal</a></li>
+                <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
+                <li><a href="{{ route('site.contato') }}">Contato</a></li>
+            </ul>
+        </div>
+    </div>
 
-            <div class="informacao-pagina">
-                <p>O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os negócios da sua empresa.</p>
-                <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
-            </div>  
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina">
+            <h1>Olá, eu sou o Super Gestão</h1>
         </div>
 
-        <div class="rodape">
-            <div class="redes-sociais">
-                <h2>Redes sociais</h2>
-                <img src="img/facebook.png">
-                <img src="img/linkedin.png">
-                <img src="img/youtube.png">
-            </div>
-            <div class="area-contato">
-                <h2>Contato</h2>
-                <span>(11) 3333-4444</span>
-                <br>
-                <span>supergestao@dominio.com.br</span>
-            </div>
-            <div class="localizacao">
-                <h2>Localização</h2>
-                <img src="img/mapa.png">
-            </div>
+        <div class="informacao-pagina">
+            <p>O Super Gestão é o sistema online de controle administrativo que pode transformar e potencializar os negócios da sua empresa.</p>
+            <p>Desenvolvido com a mais alta tecnologia para você cuidar do que é mais importante, seus negócios!</p>
+        </div>  
+    </div>
+
+    <div class="rodape">
+        <div class="redes-sociais">
+            <h2>Redes sociais</h2>
+            <img src="img/facebook.png">
+            <img src="img/linkedin.png">
+            <img src="img/youtube.png">
         </div>
-    </body>
-</html>
+        <div class="area-contato">
+            <h2>Contato</h2>
+            <span>(11) 3333-4444</span>
+            <br>
+            <span>supergestao@dominio.com.br</span>
+        </div>
+        <div class="localizacao">
+            <h2>Localização</h2>
+            <img src="img/mapa.png">
+        </div>
+    </div>
+@endsection

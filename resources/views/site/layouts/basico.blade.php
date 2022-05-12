6e3bff99957e4e4ccd774052fe55c570cc446cc4
unknown <vitomartinsalmeida@gmail.com>
2022-05-12 14:28:03 -0300

    variavel atraves do yield

--- resources/views/site/layouts/basico.blade.php
+++ resources/views/site/layouts/basico.blade.php
@@ -1,13 +1,27 @@
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>Super Gestão - Sobre Nós</title>
+        <title>Super Gestão - @yield('titulo')</title>
         <meta charset="utf-8">
 
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
     </head>
 
     <body>
+        <div class="topo">
+
+            <div class="logo">
+                <img src="img/logo.png">
+            </div>
+
+            <div class="menu">
+                <ul>
+                    <li><a href="{{ route('site.index') }}">Principal</a></li>
+                    <li><a href="{{ route('site.sobrenos') }}">Sobre Nós</a></li>
+                    <li><a href="{{ route('site.contato') }}">Contato</a></li>
+                </ul>
+            </div>
+        </div>
         @yield('conteudo')
     </body>
 </html>

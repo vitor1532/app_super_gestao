4dc63d8a11e607afcd321814616436a29de45a0d
unknown <vitomartinsalmeida@gmail.com>
2022-05-18 11:53:55 -0300

    criando tabela com relação muitos para muitos

---
+++ database/migrations/2022_05_18_144856_alter_fornecedores_nova_coluna_site_com_after.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterFornecedoresNovaColunaSiteComAfter extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('fornecedores', function(Blueprint $table) {
+            $table->string('site', 150);
+        })
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}

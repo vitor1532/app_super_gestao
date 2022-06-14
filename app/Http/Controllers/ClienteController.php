0d9665227e7bdfb2b1bea5b8b1263d52ba1d2f82
unknown <vitomartinsalmeida@gmail.com>
2022-06-14 16:49:51 -0300

    configurando controladores para views (ainda inexistentes)

--- app/Http/Controllers/ClienteController.php
+++ app/Http/Controllers/ClienteController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class ClienteController extends Controller
 {
     public function index() {
-        echo 'cliente';
+        return view('app.cliente');
     }
 }
--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -10,6 +10,10 @@ class FornecedorController extends Controller
 {
 
     public function index() {
+        return view('app.fornecedor');
+    }
+
+    public function index2() {
 
         $fornecedores = Fornecedor::all();
 

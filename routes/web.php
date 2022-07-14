2a74cc86fc20b290d9b06844736c79ef34bbd66a
unknown <vitomartinsalmeida@gmail.com>
2022-07-14 14:20:59 -0300

    implementando views de pedido-produto

--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,9 @@ Route::middleware('atenticar:padrao,visitante')->prefix('/app')->group(function(
     Route::resource('pedido', 'PedidoController');
 
     //pedido_produto
-    Route::resource('pedido-produto', 'PedidoProdutoController');
+    //Route::resource('pedido-produto', 'PedidoProdutoController');
+    Route::get('pedido-produto/create/{pedido}', 'PedidoProdutoController@create')->name('pedido-produto.create');
+    Route::post('pedido-produto/store/{pedido}', 'PedidoProdutoController@store')->name('pedido-produto.store');
 
 });
 
